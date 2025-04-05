8b78886ed567e1fe8f8525f47c757c4ecdafb41e
chefdude99 <chefdude.1999@gmail.com>
2025-04-05 00:31:44 +0000

    proyecto adelantado

--- src/app.py
+++ src/app.py
@@ -6,7 +6,7 @@ from flask import Flask, request, jsonify, url_for
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from datastructures import FamilyStructure
-#from models import Person
+# from models import Person
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
@@ -16,27 +16,58 @@ CORS(app)
 jackson_family = FamilyStructure("Jackson")
 
 # Handle/serialize errors like a JSON object
+
+
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
 
 # generate sitemap with all your endpoints
+
+
 @app.route('/')
 def sitemap():
     return generate_sitemap(app)
+
 
 @app.route('/members', methods=['GET'])
 def handle_hello():
 
     # this is how you can use the Family datastructure by calling its methods
     members = jackson_family.get_all_members()
-    response_body = {
-        "hello": "world",
-        "family": members
-    }
-
+    response_body = members
 
     return jsonify(response_body), 200
+
+
+@app.route('/member', methods=['POST'])
+def add_member():
+    body = request.get_json()
+    if 'first_name' not in body:
+        return jsonify({'mensaje': 'El campo first_name debe llenarse'}), 400
+    if 'age' not in body:
+        return jsonify({'mensaje': 'El campo age debe llenarse'}), 400
+    if 'lucky_numbers' not in body:
+        return jsonify({'mensaje': 'El campo lucky_numbers debe lenarse'}), 400
+
+    print(body)
+
+    new_member_data = {
+        'id': jackson_family._generateId(),
+        'first_name': body['first_name'],
+        'last_name': jackson_family.last_name,
+        'age': body['age'],
+        'lucky_numbers': body['lucky_numbers']
+    }
+    new_member = jackson_family.add_member(new_member_data)
+    print(new_member_data)
+    return jsonify(new_member)
+
+
+@app.route('/member/<int:id>', methods=['GET'])
+def call_member():
+    body = request.get_json()
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
